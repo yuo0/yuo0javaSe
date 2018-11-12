34e629de9fb3de3d52711b6e5ca9d9eff473961d
yuo0 <1254712628@qq.com>
2018-11-12 10:34:05 +0800

    封装

---
+++ javaone/src/javaone/Fenzhuang0.java
@@ -0,0 +1,13 @@
+package javaone;
+
+public class Fenzhuang0
+{
+    public static void main(String[] args)
+	{   //實例化對象
+		Fenzhuang a2=new Fenzhuang();
+		a2.setName("陈传磊");
+		a2.setAge(200);
+		Fenzhuang.add="藍京";
+		System.out.println("名字："+a2.getName()+",年龄:"+a2.getAge()+",地址："+Fenzhuang.add);
+	}
+}
